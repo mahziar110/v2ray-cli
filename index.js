1a920e733358358bbec25b6f8da2825b5f3b86f2
Mahziar Eghdami <mahziar.jordan@gmail.com>
2024-01-22 14:32:47 +0330

    js file removed

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Vless_1 = require("./src/helpers/protocols/Vless");
-const config = 'vless://76433b5c-1e11-4bd9-9158-c324157ff896@cdn.lionetsub.online:443?security=tls&type=grpc&host=&headerType=&serviceName=Smile&sni=fal.lionetsub.online&fp=chrome&alpn=#%F0%9F%87%A9%F0%9F%87%AA%20Falkenstein';
-new Vless_1.Vless(config);
